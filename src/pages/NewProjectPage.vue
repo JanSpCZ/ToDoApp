d852bacacec3bb6db0a5a4587f4d53f68a7643b0
unknown <jan.spicka147@seznam.cz>
2024-08-21 12:33:09 +0200

    trivialities

--- src/pages/NewProjectPage.vue
+++ src/pages/NewProjectPage.vue
@@ -49,7 +49,6 @@ export default {
             this.$store.dispatch(action, body).then(() => {
                 this.$router.push("/projects")
             })
-            console.log(body)
         }
     }
 }
